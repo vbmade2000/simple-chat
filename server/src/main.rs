f51b802859c7856ed21254bcf52798042b7cf563
Malhar Vora <mlvora.2010@gmail.com>
2024-10-28 17:54:20 +0530

    Add common package

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,6 @@ mod server;
 
 #[tokio::main]
 async fn main() {
-    let server = SimpleChatServer::new("127.0.0.1:8080".to_string());
+    let server = SimpleChatServer::new("127.0.0.1:8090".to_string());
     server.start().await;
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,8 @@
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
+use common::{extract_parts, messages};
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    io::{AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter},
     net::{tcp::WriteHalf, TcpListener, TcpStream},
     sync::{
         mpsc::{channel, Receiver, Sender},
@@ -9,18 +10,6 @@ use tokio::{
     },
 };
 
-/// A simple chat server
-/*
-    Protocol:
-    1. <JOIN><SPACE><USERNAME>
-       Join the room with the given username. Username has to be unique.
-    2. <LEAVE>
-       Leave the room.
-    3. <MSG><SPACE><MESSAGE TEXT>
-       Send regular message to all users in the room.
-    4. <ERROR><SPACE><ERROR-TEXT>
-
-*/
 pub struct SimpleChatServer {
     /// The address the server is listening on
     pub address: Arc<String>,
@@ -64,21 +53,26 @@ impl SimpleChatServer {
         println!("Stopping server on {}", self.address);
     }
 
+    // BufWriter<WriteHalf<'_>>
     // Handles joining of a new user. Returns true/false based on user joining success.
-    async fn handle_join_command(
+    async fn handle_join_command<T>(
         users: Arc<Mutex<HashMap<String, Sender<String>>>>,
-        writer: &mut BufWriter<WriteHalf<'_>>,
+        writer: &mut T,
         line: &mut String,
         my_username: &mut String,
         data: &str,
         client_address: SocketAddr,
         tx: Sender<String>,
-    ) -> bool {
+    ) -> bool
+    where
+        T: AsyncWrite + std::marker::Unpin,
+    {
         if users.lock().await.contains_key(data) {
             let _ = writer
-                .write_all("Username already taken. Please try another username.\n".as_bytes())
+                .write_all(format!("<{}>\n", messages::DUPLICATE_USER).as_bytes())
                 .await;
             let _ = writer.flush().await;
+
             println!("Duplicate username received: {}", data);
             line.clear(); // Clear the buffer for safety.
             return false;
@@ -89,6 +83,13 @@ impl SimpleChatServer {
             "User {:?} from {:?} has joined the chat",
             &data, &client_address
         );
+
+        // Send ACK for user joining.
+        let _ = writer
+            .write_all(format!("<{}> {}\n", messages::USER_JOINED, &data).as_bytes())
+            .await;
+        let _ = writer.flush().await;
+
         line.clear(); // Clear the buffer for safety.
         true
     }
@@ -116,6 +117,7 @@ impl SimpleChatServer {
         line: &mut String,
         data: &str,
         client_address: &SocketAddr,
+        sender_username: &str,
     ) {
         // User should not be able to send message if not joined.
         if my_username.is_empty() {
@@ -131,13 +133,15 @@ impl SimpleChatServer {
             return;
         }
 
-        println!("Sending message to all users: {:?}", &line);
+        println!("- Sending message to all users: {:?}", &line);
         for (username, sender) in users.lock().await.iter() {
-            println!("- Sending message to user: {:?}", &username);
             if username != my_username {
-                let _ = sender
-                    .send(format!("<{:?}> {:?}", my_username, data.to_string()))
-                    .await;
+                // println!("- Sending message to user: {:?}", &username);
+                println!("User: {}", &sender_username);
+                println!("data: {}", &data);
+                let usr_msg = format!("<{}> {} {}\n", messages::USER_MSG, sender_username, data);
+                println!("Sending message: {:?}", &usr_msg);
+                let _ = sender.send(usr_msg).await;
             }
         }
     }
@@ -148,52 +152,63 @@ impl SimpleChatServer {
         users: Arc<Mutex<HashMap<String, Sender<String>>>>,
     ) {
         println!("Received connection from: {:?}", &client_address);
-
+        stream
+            .set_nodelay(true)
+            .expect("ERROR: Unable to set TCP_NODELAY");
         let (reader, writer) = stream.split();
         let mut reader = BufReader::new(reader);
         let mut writer = BufWriter::new(writer);
 
         // Send welcome message to the client
-        let _ = writer
-            .write_all("Welcome to the Simple Chat System\n".as_bytes())
-            .await;
-        let _ = writer.flush().await;
+        // let _ = writer
+        //     .write_all(format!("{} <server>\n", messages::WELCOME_MSG).as_bytes())
+        //     .await;
+        // let _ = writer.flush().await;
+
         let (tx, mut rx) = channel::<String>(1000);
 
         let mut line = String::new();
         let mut my_username = String::new();
 
+        /*
+           IMP:
+           reader.read_line() method expects \n at the end of the message to mark it as line. Without it, it will wait indefinitely.
+        */
+        println!("Starting loop");
         loop {
             tokio::select! {
                 result = reader.read_line(&mut line) => {
                     match result {
                         Ok(n) => {
+                            println!("Line received from {:?}: {:?}", &client_address, &line);
                             if n == 0 {
                                 println!("Connection closed by client {:?}", &client_address);
                                 break;
                             }
+
+                            if line.trim() == "\n" {
+                                println!("Received new line character. Ignoring.");
+                                continue;
+                            }
+
                             println!("Received network message from {:?}: {:?}", &client_address, &line);
 
-                            // Split the received line to get command and rest of the text.
-                            // Check Protocol section at the top of the file.
-                            let lines = line.split(" ").collect::<Vec<&str>>();
-                            let command = lines[0].trim().to_lowercase();
-                            let data = line[command.len()..].trim().to_string();
+                            let (command, username, data) = extract_parts(&line);
 
                             // Handle joining of the user
-                            if command == "<join>" {
+                            if command == messages::JOIN_USER {
                                 Self::handle_join_command(users.clone(), &mut writer, &mut line, &mut my_username, &data, client_address, tx.clone()).await;
                             }
                             // Handle leaving of the user
-                            else if command == "<leave>" {
+                            else if command == messages::LEAVE_USER {
                                 Self::handle_leave_command(users.clone(), &mut rx, &data, &client_address).await;
                             }
                             // Handle user messages
-                            else if command ==  "<msg>" {
-                                Self::handle_user_messages(users.clone(), &my_username, &mut writer, &mut line, &data, &client_address).await;
+                            else if command ==  messages::USER_MSG {
+                                Self::handle_user_messages(users.clone(), &my_username, &mut writer, &mut line, &data, &client_address, &username).await;
                             } else {
                                 println!("Invalid command received: {:?}", &line);
-                                let _ = writer.write_all("Invalid command received. Please try again.\n".as_bytes()).await;
+                                let _ = writer.write_all(format!("{}", messages::INVALID_CMD).as_bytes()).await;
                                 let _ = writer.flush().await;
                             }
 
@@ -223,4 +238,72 @@ impl SimpleChatServer {
             }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use tokio::io::{self, AsyncReadExt};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_handle_leave_command() {
+        // Input
+        let data: &str = "testuser";
+        let (tx, mut rx) = channel::<String>(1);
+        let mut users_map = HashMap::new();
+        users_map.insert(data.to_string(), tx);
+
+        let client_address = SocketAddr::new("127.0.0.1".parse().unwrap(), 9875);
+        let users = Arc::new(Mutex::new(users_map));
+
+        // Call
+        SimpleChatServer::handle_leave_command(users.clone(), &mut rx, data, &client_address).await;
+        let users = users.clone();
+
+        // Assertion
+        assert!(rx.is_closed());
+        assert_eq!(users.lock().await.len(), 0);
+    }
+
+    // async fn test_handle_join_command_user_exists() {
+    //     // async fn handle_join_command(
+    //     //     users: Arc<Mutex<HashMap<String, Sender<String>>>>,
+    //     //     writer: &mut BufWriter<WriteHalf<'_>>,
+    //     //     line: &mut String,
+    //     //     my_username: &mut String,
+    //     //     data: &str,
+    //     //     client_address: SocketAddr,
+    //     //     tx: Sender<String>,
+    //     // )
+    //     use std::io::Cursor;
+
+    //     // Input
+    //     let existing_username: &str = "testuser";
+    //     let (tx, _rx) = channel::<String>(1);
+
+    //     let mut users_map = HashMap::new();
+    //     users_map.insert(existing_username.to_string(), tx.clone());
+
+    //     let client_address = SocketAddr::new("127.0.0.1".parse().unwrap(), 9875);
+
+    //     // let buffer = Vec::new();
+    //     let (client, mut server) = io::duplex(64);
+    //     let (reader, writer) = tokio::io::split(server);
+    //     let mut buf_writer = BufWriter::new(writer);
+    //     let mut line = String::from("test line from user");
+    //     let mut my_username = String::from("myuser");
+    //     let users =  Arc::new(Mutex::new(users_map));
+
+    //     // Call
+
+    //     SimpleChatServer::handle_join_command(users, &mut buf_writer, &mut line, &mut my_username, existing_username, client_address, tx).await;
+    //     let mut output = String::new();
+    //     writer.read_to_string(&mut output).await.unwrap();
+
+    //     // Assertion
+    //     assert!(line.is_empty());
+    //     assert_eq!(output, "Hello");
+    // }
 }
